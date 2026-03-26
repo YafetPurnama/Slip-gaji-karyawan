ea40acc36388fcdd6de7a7d609aad1a31cbd5bb6
Yafet Purnama <joshuapurnama123@gmail.com>
2026-03-26 23:18:17 +0700

    Perubahan Footer Pembuat

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white mt-auto">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Teddy Corp {{ date('Y') }}</span>
+            <span>Copyright &copy; Yafet Purnama Developer {{ date('Y') }}</span>
         </div>
     </div>
 </footer>
